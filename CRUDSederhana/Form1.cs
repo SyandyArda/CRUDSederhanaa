e392ffdfb2a03bbe9cdbbc21f82efa28d9c86c19
SyandyArda <syandi.2901@gmail.com>
2025-03-21 16:22:43 +0700

    ganti server

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,9 @@ namespace CRUDSederhana
 {
     public partial class Form1 : Form
     {
-        // Ganti Server
+        // Ganti "SERVER" sesuai dengan SQL Server Anda
         private string connectionString = "Data Source=ARSYAN\\SYANDY_ARDA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
+        
         public Form1()
         {
             InitializeComponent();
