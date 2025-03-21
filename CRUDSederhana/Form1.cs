2696cea60c1774b9e8397fddc3f9e186c192c817
SyandyArda <syandi.2901@gmail.com>
2025-03-21 16:21:13 +0700

    Add project files.

---
+++ CRUDSederhana/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDSederhana
+{
+    public partial class Form1 : Form
+    {
+        // Ganti Server
+        private string connectionString = "Data Source=ARSYAN\\SYANDY_ARDA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //event saat form pertama kali dimuat
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData()
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
